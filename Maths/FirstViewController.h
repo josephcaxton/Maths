22f3534084ef2bfb18da4dbfeb10c637209a9b45
Joseph caxton-Idowu <Joseph@Joseph-caxton-Idowus-iMac.local>
2011-10-28 10:39:57 +0100

    Initial Commit

---
+++ Maths/FirstViewController.h
@@ -0,0 +1,13 @@
+//
+//  FirstViewController.h
+//  Maths
+//
+//  Created by Joseph caxton-Idowu on 28/10/2011.
+//  Copyright (c) 2011 caxtonidowu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FirstViewController : UIViewController
+
+@end
---
+++ Maths/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Maths
+//
+//  Created by Joseph caxton-Idowu on 28/10/2011.
+//  Copyright (c) 2011 caxtonidowu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
