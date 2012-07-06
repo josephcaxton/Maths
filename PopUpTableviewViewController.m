1649d185beb01369306f82e368c753165a821e82
Joseph caxton-Idowu <Joseph@Joseph-caxton-Idowus-iMac.local>
2012-07-06 17:28:44 +0100

    icon

--- PopUpTableviewViewController.m
+++ PopUpTableviewViewController.m
@@ -450,9 +450,10 @@ self.contentSizeForViewInPopover = CGSizeMake(108,400);
         NSString *UrlString = @"http://itunes.apple.com/us/app/gcse-maths-questions./id461348306?ls=1&mt=8";
         
         TWTweetComposeViewController *tweetSheet = [[TWTweetComposeViewController alloc] init];
-        [tweetSheet setInitialText:@"Checkout LearnersCloud maths app. Quality Maths questions and answers. :)"];
+        [tweetSheet setInitialText:@"Checkout #LearnersCloud maths app. Quality Maths questions and answers. :)"];
+        [tweetSheet addImage:[UIImage imageNamed:@"Icon.png"]];
         [tweetSheet addURL:[NSURL URLWithString:UrlString]];
-        [tweetSheet addImage:[UIImage imageNamed:@"Icon"]];
+        
         
        
         
